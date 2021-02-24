d51a9171bdb73c68ea1d9105e7cd69ce297873d2
mcervantes71 <cervantes.martine@gmail.com>
2021-02-24 13:08:07 -0700

    Add new routes for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'recipes/index'
+    get 'recipes/create'
+    get 'recipes/show'
+    get 'recipes/destroy'
+  end
   root 'homepage#index'
+  get '/*path' => 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
