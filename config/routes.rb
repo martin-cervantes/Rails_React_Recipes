b43bbaf7d1bcd73a2e0bced8a8a496600a01629b
mcervantes71 <cervantes.martine@gmail.com>
2021-02-25 11:13:40 -0700

    Add Recipe to show single recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     get 'recipes/index'
-    get 'recipes/create'
-    get 'recipes/show'
-    get 'recipes/destroy'
+    post 'recipes/create'
+    get '/show/:id', to: 'recipes#show'
+    delete '/destroy/:id', to: 'recipes#destroy'
   end
+
   root 'homepage#index'
   get '/*path' => 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
